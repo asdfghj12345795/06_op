f6b450dd0405d1535280c604ab19c4841b6397fd
asdfghj12345795 <55428385+asdfghj12345795@users.noreply.github.com>
2019-10-10 16:10:37 +0900

    submit_04

--- main.c
+++ main.c
@@ -1,42 +1,15 @@
 #include<stdio.h>
 
-int sumTwo ( int a, int b)
-
+void square (int a)
 {
-	int output;
-	output = a+ b;
-	return output;
-	
-}
-
-int square (int n)
-{
-	return n*n;
-	
-}
-
-int get_max(int a, int b)
-{
-	if(a>b) return a;
-	
-	
-	return b;
+	a=a*a;
 }
 
 int main()
 {
- int x, y;
- int output;
- 
- x=2;
- y=5;
- 
- output= sumTwo(x,y);
- printf("sumtwo : %d\n", output);
- printf("square : %d\n", square(x));
- printf("get_max : %d\n", get_max(x,y));
- 
+	int a =2;
+	square(a);
+	printf("a=%d\n", a);
 }
-
 
 
