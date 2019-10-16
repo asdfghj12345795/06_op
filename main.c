4611a8ae174d81435337a5dbc6501996a9f3023b
asdfghj12345795 <55428385+asdfghj12345795@users.noreply.github.com>
2019-10-16 21:46:28 +0900

    submit_07

--- main.c
+++ main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main(void)
+
+int factorial(int n)
 {
-	int i;
-	int max=45;
-	
-	srand((unsigned)time(NULL));
-	for(i=0;i<6;i++)
-		printf("%d ", 1+rand()%max) ;
-	
-	return 0;
+	 int res=1;
+ 	 int i;
+ 	
+ 	for(i=1;i<=n;i++)
+ 	{
+	   res = res * i;
+ }
+ 	return res;
+ }
+
+ int combination(int n, int r)
+ {
+ 	int high, low;
+ 	high = factorial(n);
+ 	low = factorial(n-r)*factorial(r);
+ 	
+ 	return (high/low);
 }
+
+ 
+ int main (void)
+ {
+ 	 float result;
+	 int n, r;
+ 	
+ 	printf(" input two number for combination : ");
+ 	scanf("%d %d", &n, &r);
+ 	result= combination(n,r);
+ 	
+ 	printf("The result is %f", result);
+
+	return 0;
+
+ }
